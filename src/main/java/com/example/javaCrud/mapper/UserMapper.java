1457f2981c8b180389a5845b8073409b42b14cca
JunHatanaka <nextseason6@gmail.com>
2023-03-31 01:26:09 +0900

    Delete: うまくいかないので破棄

--- src/main/java/com/example/javaCrud/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.javaCrud.mapper;
-
-import java.util.List;
-import com.example.javaCrud.model.UserModel;
-
-public interface UserMapper {
-    int insert(UserModel model);
-    List<UserModel> selectAll();
-}
