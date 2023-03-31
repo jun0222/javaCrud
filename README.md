c6c17e7e480c9bbb584e05cc396839730a3b85b6
JunHatanaka <nextseason6@gmail.com>
2023-03-31 21:51:53 +0900

    Feature: アプリケーションコード動作OK

---
+++ README.md
@@ -0,0 +1,34 @@
+# 参考教材
+
+- [手を動かしながら学ぶ！Spring Boot 入門](https://www.techpit.jp/courses/224)
+
+# 発展教材
+
+- [実践 SpringBoot ~SpringBoot Advanced Tutorial~](https://www.techpit.jp/courses/232)
+
+# サーバー起動
+
+GUI 操作、スクショ。
+
+# 依存関係のビルド
+
+GUI 操作
+
+# 標準出力
+
+System.
+コードつきで
+
+# H2 データベースについて
+
+sqlite ではなく
+
+# CRUD
+
+## CREATE
+
+## READ
+
+## UPDATE
+
+## DELETE
--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,16 @@
 package com.example.demo.controller;
- 
- import org.springframework.stereotype.Controller;
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.ResponseBody;
- import org.springframework.ui.Model;
 
- @Controller
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+@Controller
 public class HelloController {
   @GetMapping("/hello")
   @ResponseBody
   public String helloWorld() {
-    return "Hello,World";        
+    return "Hello,World";
   }
 
   @GetMapping("/greet")
@@ -20,7 +20,7 @@ public class HelloController {
   }
 
   @GetMapping("/sayHello")
-  public String sayHello(){
+  public String sayHello() {
     return "hello";
   }
 
